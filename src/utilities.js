405f70e744c129aae208e163292cda08d8a58a8d
RinatRiyanov <correon39@gmail.com>
2017-09-08 22:01:08 +0300

    delete

--- src/utilities.js
+++
@@ -1,9 +0,0 @@
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
-export {getRandom, getRandomFromArray};
